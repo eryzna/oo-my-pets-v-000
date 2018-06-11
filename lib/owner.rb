dafc23a5a9f62cd2f9f6c6bcf42ac022751ed041
Elana Ryznar <eryzna@gmail.com>
2018-06-11 04:24:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,13 @@ class Owner
   end
 
   def sell_pets
-    pets.mood="nervous"
+    def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
   end
 
 
