6887fb449643b1c3db0074701962fbdf529f68db
Elana Ryznar <eryzna@gmail.com>
2018-06-11 03:14:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,12 @@
 class Owner
   attr_accessor :pets
+
+  @@all=[]
   def initialize
     @pets={fishes: [], cats: [], dogs: []}
-  end# code goes here
+  end
+
+  def self.all
+    @@all
+  end
 end
