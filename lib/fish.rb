670a1f5f1fe3554f2cd6116ba0bf8460a145bcba
Elana Ryznar <eryzna@gmail.com>
2018-06-11 03:11:31 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
 class Fish
+  attr_accessor :mood
+
+  def initialize(name, mood="nervous") 
+    @name=name
+    @mood=mood
+  end
+
+  def name
+    @name
+  end
   # code goes here
+end
+# code goes here
 end
